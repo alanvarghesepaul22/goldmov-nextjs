407d3a370b68ec453f12172bbc85d80244bc9b66
Alan Varghese Paul <alanvarghesepaul29@gmail.com>
2024-10-27 17:29:18 +0530

    db connection code change

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,28 +1,20 @@
-import mongoose from 'mongoose';
+
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable in .env.local');
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 async function connectToDatabase() {
-  if (cached.conn) {
-    return cached.conn;
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
   }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
-  }
-  
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
 
-export default connectToDatabase;
+export default connectToDatabase ;
